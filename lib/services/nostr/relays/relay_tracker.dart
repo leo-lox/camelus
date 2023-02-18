7393d35b2bbe0877dc23d6e3835fca2b8f941343
LeoLox <58687994+leo-lox@users.noreply.github.com>
2023-02-18 11:26:50 +0100

    wip: tracker

---
+++ lib/services/nostr/relays/relay_tracker.dart
@@ -0,0 +1,36 @@
+enum RelayTrackerAdvType {
+  kind03,
+  nip05,
+  tag,
+}
+
+class RelayTracker {
+  Map<String, Map> tracker = {};
+
+  RelayTracker() {}
+
+  /// get called when a event advertises a relay pubkey connection
+  void trackRelay(String personPubkey, String relayUrl, RelayTrackerAdvType nip,
+      int timestamp) {
+    if (tracker[personPubkey] == null) {
+      tracker[personPubkey] = {};
+    }
+    if (tracker[personPubkey]![relayUrl] == null) {
+      tracker[personPubkey]![relayUrl] = {
+        "relayUrl": relayUrl,
+      };
+    }
+
+    switch (nip) {
+      case RelayTrackerAdvType.kind03:
+        tracker[personPubkey]![relayUrl]!["lastSuggestedKind3"] = timestamp;
+        break;
+      case RelayTrackerAdvType.nip05:
+        tracker[personPubkey]![relayUrl]!["lastSuggestedNip05"] = timestamp;
+        break;
+      case RelayTrackerAdvType.tag:
+        tracker[personPubkey]![relayUrl]!["lastSuggestedBytag"] = timestamp;
+        break;
+    }
+  }
+}
