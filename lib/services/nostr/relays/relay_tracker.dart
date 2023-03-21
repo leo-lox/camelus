e33165a942fe5c500bc4ae977eab6d354d1985ed
LeoLox <58687994+leo-lox@users.noreply.github.com>
2023-03-21 20:26:18 +0100

    fix: clean up weird relayUrls

--- lib/services/nostr/relays/relay_tracker.dart
+++ lib/services/nostr/relays/relay_tracker.dart
@@ -183,13 +183,16 @@ class RelayTracker {
       if (relayUrl.isEmpty) {
         continue;
       }
-      //remove trailing slash
-      if (relayUrl.endsWith("/")) {
-        relayUrl = relayUrl.substring(0, relayUrl.length - 1);
+      if (personPubkey.isEmpty) {
+        continue;
       }
+      //clean relay url
 
-      // remove spaces
-      relayUrl = relayUrl.replaceAll(" ", "");
+      RegExp exp = RegExp(r'^(?:https?|wss?):\/\/[^\/]+');
+      String? wssDomain = exp.firstMatch(relayUrl)?.group(0);
+      if (wssDomain == null) {
+        continue;
+      }
 
       switch (nip) {
         case RelayTrackerAdvType.kind03:
