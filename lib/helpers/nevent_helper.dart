f8294fbde2f3660221749f5891c4e10e4df26168
LeoLox <58687994+leo-lox@users.noreply.github.com>
2023-04-26 13:31:05 +0200

    refactor: nevent_helper

--- lib/helpers/nevent_helper.dart
+++ lib/helpers/nevent_helper.dart
@@ -1,43 +1,59 @@
 import 'dart:convert';
-import 'dart:developer';
-
 import 'dart:typed_data';
-
 import 'package:camelus/helpers/helpers.dart';
 import 'package:camelus/helpers/tlv_helpers.dart';
-
 import 'package:hex/hex.dart';
 
 class NeventHelper {
+  final Helpers _helper = Helpers();
+
   String mapToBech32(Map<String, dynamic> map) {
-    var helper = Helpers();
-    String eventId = map["eventId"];
-    String? authorPubkey = map["authorPubkey"];
-    List<String> relays = List<String>.from(map['relays']);
+    final String eventId = map['eventId'];
+    final String? authorPubkey = map['authorPubkey'];
+    final List<String> relays = List<String>.from(map['relays']);
 
-    List<int> pubkeyInt = HEX.decode(eventId);
-    Uint8List pubkeyBytes = Uint8List.fromList(pubkeyInt);
+    final List<TLV> tlvList = _generateTlvList(eventId, authorPubkey, relays);
+    final String dataString = HEX.encode(TlvUtils.encode(tlvList));
 
-    List<TLV> tlvList = [];
-    tlvList.add(TLV(type: 0, length: 32, value: pubkeyBytes));
+    return _helper.encodeBech32(dataString, 'nevent');
+  }
 
-    for (int i = 0; i < relays.length; i++) {
-      var relay = relays[i];
-      var relayBytes = Uint8List.fromList(ascii.encode(relay));
-      tlvList.add(TLV(type: 1, length: relayBytes.length, value: relayBytes));
-    }
+  /// Generates a list of TLV objects
+  List<TLV> _generateTlvList(
+      String eventId, String? authorPubkey, List<String> relays) {
+    final List<TLV> tlvList = [];
+    tlvList.add(_generateEventIdTlv(eventId));
+
+    tlvList.addAll(relays.map(_generateRelayTlv));
 
     if (authorPubkey != null) {
-      List<int> pubkeyInt = HEX.decode(authorPubkey);
-      Uint8List pubkeyBytes = Uint8List.fromList(pubkeyInt);
-      tlvList.add(TLV(type: 2, length: 32, value: pubkeyBytes));
+      tlvList.add(_generateAuthorPubkeyTlv(authorPubkey));
     }
 
-    Uint8List bytes = TlvUtils.encode(tlvList);
-    String dataString = HEX.encode(bytes);
+    return tlvList;
+  }
 
-    String bech32 = helper.encodeBech32(dataString, "nevent");
+  // specified in https://github.com/nostr-protocol/nips/blob/master/19.md
 
-    return bech32;
+  /// TLV type 0
+  /// [eventId] must be 32 bytes long
+  TLV _generateEventIdTlv(String eventId) {
+    final Uint8List eventIdBytes = Uint8List.fromList(HEX.decode(eventId));
+    return TLV(type: 0, length: 32, value: eventIdBytes);
+  }
+
+  /// TLV type 1
+  /// [relay] must be a string
+  TLV _generateRelayTlv(String relay) {
+    final Uint8List relayBytes = Uint8List.fromList(ascii.encode(relay));
+    return TLV(type: 1, length: relayBytes.length, value: relayBytes);
+  }
+
+  /// TLV type 2
+  /// [authorPubkey] must be 32 bytes long
+  TLV _generateAuthorPubkeyTlv(String authorPubkey) {
+    final Uint8List authorPubkeyBytes =
+        Uint8List.fromList(HEX.decode(authorPubkey));
+    return TLV(type: 2, length: 32, value: authorPubkeyBytes);
   }
 }
