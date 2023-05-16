946f9412e5a3200a012aa04a34cd2cde350f792a
LeoLox <58687994+leo-lox@users.noreply.github.com>
2023-05-16 17:34:12 +0200

    test: relays_picker mock shared preferences

--- test/widget_integration_test/relays/relays_picker_test.dart
+++ test/widget_integration_test/relays/relays_picker_test.dart
@@ -1,14 +1,23 @@
 import 'package:camelus/services/nostr/relays/relays_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:flutter_test/flutter_test.dart';
 import 'package:test/test.dart';
 
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
 void main() {
+  SharedPreferences.setMockInitialValues({}); //set values here
+
   //TestWidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
   group('picker EmptyTracker', () {
     var picker = RelaysPicker();
     picker.relayTracker.tracker = {};
+
+    final mockSharedPreferences = MockSharedPreferences();
+    when(mockSharedPreferences.getString('')).thenReturn('mock');
 
     test('emptyTracker:NoPeopleLeft', () {
       picker.relayTracker.tracker = {};
@@ -42,6 +51,8 @@ void main() {
   });
 
   group("picker Populated", () {
+    final mockSharedPreferences = MockSharedPreferences();
+    when(mockSharedPreferences.getString('')).thenReturn('mock');
     var picker = RelaysPicker();
 
     var now = DateTime.now().millisecondsSinceEpoch ~/ 1000;
