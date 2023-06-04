3bb67e1dcbd12b7926907fb83826f10619fdc641
LeoLox <58687994+leo-lox@users.noreply.github.com>
2023-06-04 16:56:24 +0200

    to nostrNote()

--- lib/db/entities/db_note_view_base.dart
+++ lib/db/entities/db_note_view_base.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:camelus/models/nostr_note.dart';
+import 'package:camelus/models/nostr_tag.dart';
+
 abstract class DbNoteViewBase {
   String id;
   String pubkey;
@@ -27,5 +30,40 @@ abstract class DbNoteViewBase {
   @override
   String toString() {
     return 'DbNostrNote{id: $id, pubkey: $pubkey, created_at: $created_at, kind: $kind, content: $content, sig: $sig, tag_types: $tag_types, tag_values: $tag_values, tag_recommended_relays: $tag_recommended_relays, tag_markers: $tag_markers}';
+  }
+
+  NostrNote toNostrNote() {
+    return NostrNote(
+        id,
+        pubkey,
+        created_at,
+        kind,
+        content,
+        sig,
+        toNostrTags(
+            tag_types: tag_types!,
+            tag_values: tag_values!,
+            tag_recommended_relays: tag_recommended_relays!,
+            tag_markers: tag_markers!));
+  }
+
+  List<NostrTag> toNostrTags(
+      {required String tag_types,
+      required String tag_values,
+      required String tag_recommended_relays,
+      required String tag_markers}) {
+    List<NostrTag> tags = [];
+    List<String> types = tag_types.split(',');
+    List<String> values = tag_values.split(',');
+    List<String> recommended_relays = tag_recommended_relays.split(',');
+    List<String> markers = tag_markers.split(',');
+    for (int i = 0; i < types.length; i++) {
+      tags.add(NostrTag(
+          type: types[i],
+          value: values[i],
+          recommended_relay: recommended_relays[i],
+          marker: markers[i]));
+    }
+    return tags;
   }
 }
