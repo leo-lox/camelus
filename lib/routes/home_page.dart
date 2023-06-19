ef583c58c12b02dc7343e20f84018a331652487c
LeoLox <58687994+leo-lox@users.noreply.github.com>
2023-06-19 15:28:01 +0200

    tooltip

--- lib/routes/home_page.dart
+++ lib/routes/home_page.dart
@@ -176,6 +176,7 @@ class _HomePageState extends ConsumerState<HomePage> {
                   )
               ],
             ),
+            tooltip: _selectedIndex == 0 ? "scroll to top" : "home",
             label: "home",
           ),
           BottomNavigationBarItem(
