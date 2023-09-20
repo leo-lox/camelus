3189bd225422236fe75a2d39d2968591396c8b7f
LeoLox <58687994+leo-lox@users.noreply.github.com>
2023-09-20 14:32:46 +0200

    wip: db settings

---
+++ lib/db/entities/db_settings.dart
@@ -0,0 +1,21 @@
+import 'package:isar/isar.dart';
+
+part 'db_settings.g.dart';
+
+@collection
+class DbSettings {
+  Id id = Isar.autoIncrement;
+}
+
+@embedded
+class DbManualRelay {
+  String? url;
+  bool? read;
+  bool? write;
+
+  DbManualRelay({
+    this.url,
+    this.read,
+    this.write,
+  });
+}
