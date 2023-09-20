c2a6adf7aac56a5e8c6b705435a2915e7df01742
LeoLox <58687994+leo-lox@users.noreply.github.com>
2023-09-20 14:32:36 +0200

    deleted legacy code

--- lib/services/nostr/relays/relays_injector.dart
+++ lib/services/nostr/relays/relays_injector.dart
@@ -1,12 +1,11 @@
 import 'package:camelus/services/nostr/relays/relay_tracker.dart';
-import 'package:camelus/services/nostr/relays/relays.dart';
+
 import 'package:camelus/services/nostr/relays/relays_ranking.dart';
 
 class RelaysInjector {
   static final _singleton = RelaysInjector._internal();
   static RelaysInjector? _injector;
 
-  Relays? _relays;
   RelayTracker? _relayTracker;
   RelaysRanking? _relaysRanking;
 
@@ -18,10 +17,6 @@ class RelaysInjector {
 
   static void configure(RelaysInjector injector) {
     _injector = injector;
-  }
-
-  Relays get relays {
-    return _relays ??= Relays();
   }
 
   RelayTracker get relayTracker {
