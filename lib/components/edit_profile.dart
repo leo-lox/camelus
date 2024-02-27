2def481ced010e08ddfefdd7367c1f925fad5a05
Alex <alexander.martens@code.berlin>
2024-02-27 23:06:09 +0000

    TextField and borders

--- lib/components/edit_profile.dart
+++ lib/components/edit_profile.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:camelus/config/palette.dart';
+import 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,19 +14,64 @@ class EditProfile extends ConsumerStatefulWidget {
 }
 
 class _EditProfileState extends ConsumerState<EditProfile> {
+  final myController = TextEditingController();
+  readText() {
+    log(myController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(),
+      padding: const EdgeInsets.all(3),
       child: Column(
         children: [
+          Text(
+            "welcome to",
+            style: TextStyle(
+              color: Palette.extraLightGray,
+              fontSize: MediaQuery.of(context).size.width / 33,
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              SizedBox(
+                width: double.infinity,
+                child: Container(
+                 /// color: Colors.red,
+                  child: Text(
+                    "Name",
+                    textAlign: TextAlign.left,
+                    
+                  ),
+                ),
+              ),
+            ],
+          ),
           TextField(
+            decoration: InputDecoration(
+              hintText: "",
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1, //<-- SEE HERE
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+          TextField(
+            controller: myController,
+            decoration: const InputDecoration(
+              hintText: "Please Type in",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const TextField(
             decoration: InputDecoration(
               hintText: "Please Type in",
               border: OutlineInputBorder(),
             ),
           ),
-          TextButton(onPressed: () => {}, child: Text("Go"))
+          TextButton(onPressed: () => {readText()}, child: const Text("Go"))
         ],
       ),
     );
