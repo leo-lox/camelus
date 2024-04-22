4ccd8cce8885b616bdb5f18b1fdbd4dfc37db0f8
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-04-22 18:01:33 +0200

    refactor nostr_band

--- lib/data_layer/data_sources/api_nostr_band_data_source.dart
+++ lib/data_layer/data_sources/api_nostr_band_data_source.dart
@@ -5,37 +5,28 @@ import 'package:camelus/data_layer/models/nostr_band_people_model.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class ApiNostrBandDataSource {
-  Future<NostrBandPeopleModel?> getTrendingProfiles() async {
-    // cached network request from https://api.nostr.band/v0/trending/profiles
-
+  Future<T> _fetchData<T>(String type, String key,
+      T Function(Map<String, dynamic>) fromJson) async {
     var file = await DefaultCacheManager().getSingleFile(
-      'https://camelus.app/api/v1/nostr-band-cache?type=profiles&limit=10',
-      key: 'trending_profiles_nostr_band',
-      // 2 hours
+      'https://camelus.app/api/v1/nostr-band-cache?type=$type&limit=10',
+      key: key,
       headers: {'Cache-Control': 'max-age=7200'},
     );
     var result = await file.readAsString();
     if (result.isEmpty) {
-      return null;
+      throw Exception('No data');
     }
     var json = jsonDecode(result);
+    return fromJson(json);
+  }
 
-    return NostrBandPeopleModel.fromJson(json);
+  Future<NostrBandPeopleModel?> getTrendingProfiles() async {
+    return _fetchData('profiles', 'trending_profiles_nostr_band',
+        NostrBandPeopleModel.fromJson);
   }
 
   Future<NostrBandHashtagsModel?> getTrendingHashtags() async {
-    //var file = await DefaultCacheManager().getSingleFile(url);
-    var file = await DefaultCacheManager().getSingleFile(
-      'https://camelus.app/api/v1/nostr-band-cache?type=hashtags&limit=10',
-      key: 'trending_hashtags_nostr_band',
-      // 2 hours min
-      headers: {'Cache-Control': 'max-age=7200'},
-    );
-    var result = await file.readAsString();
-    if (result.isEmpty) {
-      return null;
-    }
-    var json = jsonDecode(result);
-    return NostrBandHashtagsModel.fromJson(json);
+    return _fetchData('hashtags', 'trending_hashtags_nostr_band',
+        NostrBandHashtagsModel.fromJson);
   }
 }
