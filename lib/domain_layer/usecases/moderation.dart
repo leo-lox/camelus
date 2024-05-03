a00034f20d2e459303b99091a61e33869e05f9da
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-05-03 17:19:14 +0200

    delete block mute provider => moderation Provider + usecase

---
+++ lib/domain_layer/usecases/moderation.dart
@@ -0,0 +1,17 @@
+class Moderation {
+  Future<void> muteUser(String npub) async {
+    throw UnimplementedError();
+  }
+
+  Future<void> unmuteUser(String npub) async {
+    throw UnimplementedError();
+  }
+
+  Future<bool> isMuted(String npub) async {
+    throw UnimplementedError();
+  }
+
+  Stream<List<String>> getMuted(String npub) {
+    throw UnimplementedError();
+  }
+}
