07deb5c8153ac1c361abedd74f1db201b6142ae8
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-05-03 17:15:52 +0200

    replaced follow provider with follow useCase

---
+++ lib/domain_layer/usecases/follow.dart
@@ -0,0 +1,17 @@
+class Follow {
+  Future<void> followUser(String npub) async {
+    throw UnimplementedError();
+  }
+
+  Future<void> unfollowUser(String npub) async {
+    throw UnimplementedError();
+  }
+
+  Future<bool> isFollowing(String npub) async {
+    throw UnimplementedError();
+  }
+
+  Stream<List<String>> getFollowers(String npub) {
+    throw UnimplementedError();
+  }
+}
--- lib/presentation_layer/providers/following_provider.dart
+++ lib/presentation_layer/providers/following_provider.dart
@@ -1,16 +1,8 @@
-import 'package:camelus/presentation_layer/providers/database_provider.dart';
-import 'package:camelus/presentation_layer/providers/key_pair_provider.dart';
-import 'package:camelus/presentation_layer/providers/relay_provider.dart';
-import 'package:camelus/services/nostr/metadata/following_pubkeys.dart';
+import 'package:camelus/domain_layer/usecases/follow.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-var followingProvider = Provider<FollowingPubkeys>((ref) {
-  final keyPair = ref.watch(keyPairProvider.future);
-  final db = ref.watch(databaseProvider.future);
-  final relays = ref.watch(relayServiceProvider);
+var followingProvider = Provider<Follow>((ref) {
+  final follow = Follow();
 
-  final followingPubkeys =
-      FollowingPubkeys(keyPair: keyPair, db: db, relays: relays);
-
-  return followingPubkeys;
+  return follow;
 });
