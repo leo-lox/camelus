106a39c0204d9d338f419498612a6cb84e8ba887
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-05-03 18:09:05 +0200

    replace feeds with usecase

--- lib/domain_layer/repositories/note_repository.dart
+++ lib/domain_layer/repositories/note_repository.dart
@@ -1,8 +1,5 @@
 import 'package:camelus/domain_layer/entities/nostr_note.dart';
-import 'package:camelus/domain_layer/entities/user_metadata.dart';
 
 abstract class NoteRepository {
   Stream<NostrNote> getAllNotes();
-
-  Stream<UserMetadata> getMetadataByPubkey(String pubkey);
 }
--- lib/domain_layer/usecases/get_notes.dart
+++ lib/domain_layer/usecases/get_notes.dart
@@ -9,4 +9,52 @@ class GetNotes {
   Stream<NostrNote> getAllNotes() {
     return _noteRepository.getAllNotes();
   }
+
+  Future<void> closeFeed(String requestId) {
+    throw UnimplementedError();
+  }
+
+  /// returns a stream of notes for given events (usually the root note id)
+  Stream<List<NostrNote>> getThreadFeed({
+    required List<String> eventIds,
+    required String requestId,
+    int? since,
+    int? until,
+    int? limit,
+  }) {
+    throw UnimplementedError();
+  }
+
+  /// returns a stream of notes for a given npub (follows of that npub)
+  Stream<List<NostrNote>> getNpubFeed({
+    required String npub,
+    required String requestId,
+    int? since,
+    int? until,
+    int? limit,
+  }) {
+    throw UnimplementedError();
+  }
+
+  /// returns a stream of notes for a given npub (follows of that npub) with replies
+  Stream<List<NostrNote>> getNpubWithRepliesFeed({
+    required String npub,
+    required String requestId,
+    int? since,
+    int? until,
+    int? limit,
+  }) {
+    throw UnimplementedError();
+  }
+
+  /// returns a stream of notes for a given npub (follows of that npub) with replies
+  Stream<List<NostrNote>> getHashtagFeed({
+    required List<String> hashtags,
+    required String requestId,
+    int? since,
+    int? until,
+    int? limit,
+  }) {
+    throw UnimplementedError();
+  }
 }
