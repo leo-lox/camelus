7df3d370dd155c41a706db2c9c0b5dba8eaca94f
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-10-31 19:41:18 +0100

    use abstract class in provider

--- lib/presentation_layer/providers/db_app_provider.dart
+++ lib/presentation_layer/providers/db_app_provider.dart
@@ -1,7 +1,8 @@
 import 'package:riverpod/riverpod.dart';
 import '../../data_layer/db/object_box_camelus/db_camelus.dart';
+import '../../domain_layer/repositories/app_db.dart';
 
-final dbAppProvider = Provider<DbAppImpl>((ref) {
-  final db = DbAppImpl();
+final dbAppProvider = Provider<AppDb>((ref) {
+  final AppDb db = DbAppImpl();
   return db;
 });
