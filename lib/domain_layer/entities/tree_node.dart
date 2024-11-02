249bb35562c1b1ea118877fe94f83896ba3b6a9a
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-11-02 17:48:12 +0100

    isLeaf, hasChildren

--- lib/domain_layer/entities/tree_node.dart
+++ lib/domain_layer/entities/tree_node.dart
@@ -1,24 +1,39 @@
 class TreeNode<T> {
-  T value;
+  final T value;
   TreeNode<T>? parent;
-  List<TreeNode<T>> children;
+  final List<TreeNode<T>> _children;
 
   bool get isFirstChild => parent?.children.first == this;
   bool get isLastChild => parent?.children.last == this;
   bool get hasSiblings => parent != null && parent!.children.length > 1;
 
-  TreeNode(this.value) : children = <TreeNode<T>>[];
+  bool get hasChildren => _children.isNotEmpty;
+  bool get isLeaf => _children.isEmpty;
+  List<TreeNode<T>> get children => List.unmodifiable(_children);
+
+  TreeNode(this.value) : _children = <TreeNode<T>>[];
 
   void addChild(TreeNode<T> child) {
-    children.add(child);
+    _children.add(child);
     child.parent = this;
+  }
+
+  void addChildren(List<TreeNode<T>> children) {
+    for (var child in children) {
+      addChild(child);
+    }
+  }
+
+  void removeChild(TreeNode<T> child) {
+    _children.remove(child);
+    child.parent = null;
   }
 
   void printTree([String prefix = '']) {
     print('$prefix${value.toString()}');
-    for (var i = 0; i < children.length; i++) {
-      var child = children[i];
-      var isLastChild = i == children.length - 1;
+    for (var i = 0; i < _children.length; i++) {
+      var child = _children[i];
+      var isLastChild = i == _children.length - 1;
       child.printTree('$prefix${isLastChild ? '└── ' : '├── '}');
     }
   }
