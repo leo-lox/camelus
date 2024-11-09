2283349436a55b8ba8f4319ab316e2cd44976e56
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-11-09 18:54:36 +0100

    better note ref state

--- lib/presentation_layer/components/note_card/note_card_reference.dart
+++ lib/presentation_layer/components/note_card/note_card_reference.dart
@@ -3,9 +3,32 @@ import 'package:camelus/config/palette.dart';
 import 'package:camelus/helpers/helpers.dart';
 import 'package:camelus/helpers/nevent_helper.dart';
 import 'package:camelus/domain_layer/entities/nostr_note.dart';
+import 'package:camelus/presentation_layer/components/note_card/skeleton_note.dart';
 import 'package:camelus/presentation_layer/providers/get_notes_provider.dart';
+import 'package:camelus/domain_layer/entities/user_metadata.dart';
+import 'package:camelus/presentation_layer/providers/metadata_provider.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rxdart/rxdart.dart';
+
+final noteAndMetadataProvider =
+    FutureProvider.family<(NostrNote, UserMetadata?), String>(
+        (ref, nostrId) async {
+  final notesProvider = ref.watch(getNotesProvider);
+  final metadataP = ref.watch(metadataProvider);
+
+  final noteStream = notesProvider.getNote(nostrId);
+  final note = await noteStream.first;
+
+  if (note == null) {
+    throw Exception('Note not found');
+  }
+
+  final metadataStream = metadataP.getMetadataByPubkey(note.pubkey);
+  final metadata = await metadataStream.first;
+
+  return (note, metadata);
+});
 
 class NoteCardReference extends ConsumerStatefulWidget {
   final String word;
@@ -20,92 +43,77 @@ class NoteCardReference extends ConsumerStatefulWidget {
 }
 
 class _NoteCardReferenceState extends ConsumerState<NoteCardReference> {
-  late Widget body = Container();
-
-  late final String? nostrId;
-
-  _fetchNoteIfUnavailable() async {
-    final notesProvider = ref.read(getNotesProvider);
-    final result = notesProvider.getNote(nostrId!);
-    if (result != null) {
-      // note exits in db
-      return;
-    }
-  }
-
-  Widget? _buildBody() {
-    //return Text("OKKKKK", style: const TextStyle(color: Palette.primary));
-
-    if (nostrId == null) {
-      return null;
-    }
-
-    return _myBody(nostrId!);
-  }
-
-  String? _getNostrId(String word) {
-    final cleanedWord = widget.word.replaceAll("nostr:", "");
-
-    if (cleanedWord.startsWith("note")) {
-      final String res;
-      try {
-        res = Helpers().decodeBech32(cleanedWord)[0];
-      } catch (e) {
-        return null;
-      }
-      return res;
-    }
-    if (cleanedWord.startsWith("nevent")) {
-      final String res;
-      try {
-        final map = NeventHelper().bech32ToMap(cleanedWord);
-        res = map["eventId"];
-        final relays = map["relays"];
-        final authorPubkey = map["authorPubkey"];
-      } catch (e) {
-        return null;
-      }
-      return res;
-    }
-    return null;
-  }
+  String? nostrId;
 
   @override
   void initState() {
     super.initState();
-
     nostrId = _getNostrId(widget.word);
-
-    _fetchNoteIfUnavailable();
-    body = _buildBody() ?? Container();
   }
 
   @override
   void didUpdateWidget(NoteCardReference oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (oldWidget.word != widget.word) {
-      body = _buildBody() ?? Container();
+      nostrId = _getNostrId(widget.word);
     }
+  }
+
+  String? _getNostrId(String word) {
+    final cleanedWord = word.replaceAll("nostr:", "");
+
+    if (cleanedWord.startsWith("note")) {
+      try {
+        return Helpers().decodeBech32(cleanedWord)[0];
+      } catch (e) {
+        return null;
+      }
+    }
+    if (cleanedWord.startsWith("nevent")) {
+      try {
+        final map = NeventHelper().bech32ToMap(cleanedWord);
+        return map["eventId"];
+      } catch (e) {
+        return null;
+      }
+    }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
-    return body;
-  }
+    if (nostrId == null) {
+      return const SizedBox.shrink();
+    }
 
-  Column _myBody(String nostrId) {
-    final notesProvider = ref.read(getNotesProvider);
-    final noteStream = notesProvider.getNote(nostrId);
-    return Column(
-      children: [
-        const SizedBox(height: 20),
-        StreamBuilder<NostrNote?>(
-          stream: noteStream,
-          builder: (context, snapshotStream) {
-            if (snapshotStream.hasData) {
-              final NostrNote note = snapshotStream.data!;
-              final Key key = UniqueKey();
-              return GestureDetector(
+    return Consumer(
+      builder: (context, ref, child) {
+        final noteAndMetadataAsync =
+            ref.watch(noteAndMetadataProvider(nostrId!));
+
+        return noteAndMetadataAsync.when(
+          loading: () => const Center(child: SkeletonNote()),
+          error: (error, stack) => Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Palette.darkGray, width: 1.0),
+            ),
+            child: const Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                child: Text(
+                  "Note not found",
+                  style: TextStyle(color: Palette.white, fontSize: 17),
+                ),
+              ),
+            ),
+          ),
+          data: (data) {
+            final (note, metadata) = data;
+            return Column(
+              children: [
+                const SizedBox(height: 20),
+                GestureDetector(
                   onTap: () {
                     Navigator.pushNamed(context, "/nostr/event", arguments: {
                       "root": note.id,
@@ -113,33 +121,23 @@ class _NoteCardReferenceState extends ConsumerState<NoteCardReference> {
                     });
                   },
                   child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Palette.darkGray, width: 1.0),
-                      ),
-                      child: NoteCard(
-                        note: note,
-                        myMetadata: null,
-                        key: key,
-                        hideBottomBar: true,
-                      )));
-            }
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Palette.darkGray, width: 1.0),
-              ),
-              child: const Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
-                  child: Text("note not found",
-                      style: TextStyle(color: Palette.white, fontSize: 17)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Palette.darkGray, width: 1.0),
+                    ),
+                    child: NoteCard(
+                      note: note,
+                      myMetadata: metadata,
+                      key: ValueKey(note.id),
+                      hideBottomBar: true,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             );
           },
-        ),
-      ],
+        );
+      },
     );
   }
 }
