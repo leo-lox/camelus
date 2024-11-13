0076acf46455f190e6322d83a7253a41ba15626b
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-11-13 12:04:21 +0100

    remove test feed from notifications

--- lib/presentation_layer/routes/notification_page.dart
+++ lib/presentation_layer/routes/notification_page.dart
@@ -1,39 +1,18 @@
-import 'package:camelus/config/palette.dart';
-import 'package:camelus/domain_layer/entities/feed_filter.dart';
-import 'package:camelus/presentation_layer/components/generic_feed.dart';
-import 'package:camelus/presentation_layer/providers/following_provider.dart';
-
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../config/palette.dart';
 
 class NotificationPage extends ConsumerWidget {
   const NotificationPage({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
-    final followP = ref.watch(followingProvider);
-
     return Scaffold(
-      backgroundColor: Palette.background,
-      appBar: AppBar(
-        title: Text("Notifications"),
-      ),
-      body: FutureBuilder(
-        future: followP.getContactsSelf(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            return GenericFeed(
-              feedFilter: FeedFilter(
-                feedId: "testfeed",
-                kinds: [1],
-                authors: snapshot.data?.contacts ?? [],
-              ),
-            );
-          }
-        },
-      ),
-    );
+        backgroundColor: Palette.background,
+        body: const Center(
+          child:
+              Text('work in progress', style: TextStyle(color: Colors.white)),
+        ));
   }
 }
