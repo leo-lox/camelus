74927e2f6999f120f23ed00caac4df7e40cc0cf0
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-11-22 19:00:13 +0100

    follow state

--- lib/presentation_layer/routes/nostr/onboarding/onboarding_starter_pack.dart
+++ lib/presentation_layer/routes/nostr/onboarding/onboarding_starter_pack.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 import '../../../../config/onboard_conf.dart';
 import '../../../../config/palette.dart';
+import '../../../../domain_layer/entities/nostr_list.dart';
 import '../../../../domain_layer/entities/onboarding_user_info.dart';
 import '../../../atoms/long_button.dart';
 import '../../../atoms/my_profile_picture.dart';
@@ -26,9 +28,12 @@ class OnboardingStarterPack extends ConsumerStatefulWidget {
 }
 
 class _OnboardingStarterPackState extends ConsumerState<OnboardingStarterPack> {
+  late List<String> selectedPubkeys;
+
   @override
   void initState() {
     super.initState();
+    selectedPubkeys = widget.userInfo.followPubkeys;
   }
 
   @override
@@ -110,7 +115,17 @@ class _OnboardingStarterPackState extends ConsumerState<OnboardingStarterPack> {
                               .toList(),
                         ),
                       ),
-                      onTap: () {},
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => OnboardingOpenStarterPack(
+                              followSet: nostrSet,
+                              selectedPubkeys: selectedPubkeys,
+                            ),
+                          ),
+                        );
+                      },
                     );
                   }).toList(),
                 );
@@ -122,11 +137,204 @@ class _OnboardingStarterPackState extends ConsumerState<OnboardingStarterPack> {
             width: 400,
             height: 40,
             child: longButton(
-              name: true ? "next" : "skip",
+              name: selectedPubkeys.isNotEmpty
+                  ? "continue with ${selectedPubkeys.length} accounts"
+                  : "select a starter pack",
               onPressed: (() {
-                widget.submitCallback([]);
+                widget.submitCallback(selectedPubkeys);
               }),
-              inverted: false,
+              disabled: selectedPubkeys.isEmpty,
+              inverted: true,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingOpenStarterPack extends ConsumerStatefulWidget {
+  final NostrSet followSet;
+  final List<String> selectedPubkeys;
+
+  const OnboardingOpenStarterPack({
+    super.key,
+    required this.followSet,
+    required this.selectedPubkeys,
+  });
+  @override
+  ConsumerState<OnboardingOpenStarterPack> createState() =>
+      _OnboardingOpenStarterPackState();
+}
+
+class _OnboardingOpenStarterPackState
+    extends ConsumerState<OnboardingOpenStarterPack> {
+  late final List<String> selectedPubkeys;
+
+  // check if nothing of the followSet is selected
+  bool get nothingOfOwnSelected => widget.followSet.elements
+      .every((element) => !selectedPubkeys.contains(element.value));
+
+  Iterable<NostrListElement> get ownSelected => widget.followSet.elements
+      .where((element) => selectedPubkeys.contains(element.value));
+  int get ownSelectedCount => ownSelected.length;
+
+  bool get allSelected => widget.followSet.elements
+      .every((element) => selectedPubkeys.contains(element.value));
+
+  @override
+  void initState() {
+    super.initState();
+    selectedPubkeys = widget.selectedPubkeys;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Palette.background,
+      appBar: AppBar(
+        backgroundColor: Palette.background,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              flex: 20,
+              child: Text.rich(
+                TextSpan(children: [
+                  TextSpan(
+                    text: widget.followSet.title ?? widget.followSet.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                        " by ${ref.watch(metadataStateProvider(widget.followSet.pubKey)).userMetadata?.name ?? "Unknown"}",
+                    style: TextStyle(
+                      color: Palette.gray,
+                      fontSize: 12,
+                    ),
+                  ),
+                ]),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Spacer(flex: 1),
+            if (allSelected)
+              longButton(
+                  name: "unselect all",
+                  onPressed: () {
+                    // unselect only own
+                    setState(() {
+                      selectedPubkeys.removeWhere((element) {
+                        return widget.followSet.elements
+                            .map((e) => e.value)
+                            .contains(element);
+                      });
+                    });
+                  })
+          ],
+        ),
+        foregroundColor: Palette.white,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: widget.followSet.elements.length,
+              itemBuilder: (context, index) {
+                final displayPubkey = widget.followSet.elements[index].value;
+                final displayMetadata = ref
+                    .watch(metadataStateProvider(displayPubkey))
+                    .userMetadata;
+                return ListTile(
+                  onTap: () {
+                    // Toggle selection
+                    setState(() {
+                      if (selectedPubkeys.contains(displayPubkey)) {
+                        selectedPubkeys.remove(displayPubkey);
+                      } else {
+                        selectedPubkeys.add(displayPubkey);
+                      }
+                    });
+                  },
+                  title: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      UserImage(
+                        imageUrl: displayMetadata?.picture,
+                        pubkey: displayPubkey,
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              displayMetadata?.name ?? "",
+                              style: const TextStyle(
+                                color: Palette.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              displayMetadata?.about ?? "",
+                              style: const TextStyle(
+                                color: Palette.gray,
+                                fontSize: 12,
+                              ),
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: Icon(
+                    selectedPubkeys.contains(displayPubkey)
+                        ? PhosphorIcons.checkCircle()
+                        : PhosphorIcons.circle(),
+                    color: Palette.white,
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            width: 400,
+            height: 40,
+            child: longButton(
+              name: nothingOfOwnSelected
+                  ? "follow all"
+                  : "follow ${ownSelectedCount} accounts",
+              onPressed: (() {
+                setState(() {
+                  if (nothingOfOwnSelected) {
+                    selectedPubkeys
+                        .addAll(widget.followSet.elements.map((e) => e.value));
+                  }
+
+                  Navigator.pop(context, selectedPubkeys);
+                });
+              }),
+              disabled: false,
+              inverted: true,
             ),
           ),
           const SizedBox(
