9488efa20d9d4d7ac976156fece44f35522ed645
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-02-27 11:15:28 +0100

    thread loading proof of concept

--- lib/domain_layer/entities/feed_event_view_model.dart
+++ lib/domain_layer/entities/feed_event_view_model.dart
@@ -4,19 +4,24 @@ import 'tree_node.dart';
 class FeedEventViewModel {
   NostrNote? rootNote;
   List<TreeNode<NostrNote>> comments;
+  Set<NostrNote> unprocessedCommentsSet = {};
 
   FeedEventViewModel({
     required this.rootNote,
     required this.comments,
+    required this.unprocessedCommentsSet,
   });
 
   copyWith({
     NostrNote? rootNote,
     List<TreeNode<NostrNote>>? comments,
+    Set<NostrNote>? unprocessedCommentsSet,
   }) {
     return FeedEventViewModel(
       rootNote: rootNote ?? this.rootNote,
       comments: comments ?? this.comments,
+      unprocessedCommentsSet:
+          unprocessedCommentsSet ?? this.unprocessedCommentsSet,
     );
   }
 }
--- lib/presentation_layer/providers/event_feed_provider.dart
+++ lib/presentation_layer/providers/event_feed_provider.dart
@@ -1,15 +1,13 @@
 import 'dart:async';
 
 import 'package:riverpod/riverpod.dart';
+import 'package:ndk/entities.dart' as ndk_entities;
+import 'package:ndk/ndk.dart' as ndk;
+import 'package:rxdart/rxdart.dart';
 
-import '../../data_layer/data_sources/dart_ndk_source.dart';
-import '../../data_layer/repositories/note_repository_impl.dart';
 import '../../domain_layer/entities/feed_event_view_model.dart';
-import '../../domain_layer/repositories/note_repository.dart';
 import '../../domain_layer/usecases/event_feed.dart';
-
-import 'event_verifier.dart';
-import 'ndk_provider.dart';
+import 'get_notes_provider.dart';
 
 /// Riverpod NotifierProvider for managing the state of a specific event feed.
 /// [String] represents the root event ID.
@@ -27,36 +25,12 @@ class EventFeedState
   StreamSubscription? _rootNoteSub;
   StreamSubscription? _commentNotesSub;
 
-  // Instance of the [EventFeed] use case for handling event-related operations.
-  late EventFeed myEventFeed;
-
-  /// Sets up the [EventFeed] by initializing required dependencies such as 
-  /// the Note Repository and Event Verifier.
-  _setupEventFeed() {
-    // Retrieves the NDK instance from the provider.
-    final ndk = ref.watch(ndkProvider);
-
-    // Retrieves the Event Verifier from the provider.
-    final eventVerifier = ref.watch(eventVerifierProvider);
-
-    // Creates a new DartNdkSource using the NDK instance.
-    final DartNdkSource dartNdkSource = DartNdkSource(ndk);
-
-    // Initializes the Note Repository with the required dependencies.
-    final NoteRepository noteRepository = NoteRepositoryImpl(
-      dartNdkSource: dartNdkSource,
-      eventVerifier: eventVerifier,
-    );
-
-    // Creates the EventFeed use case with the Note Repository.
-    myEventFeed = EventFeed(noteRepository);
-  }
-
   /// Resets the state and disposes of active resources like stream subscriptions.
   Future<void> resetStateDispose() async {
     // Resets the state to an empty FeedEventViewModel.
     state = FeedEventViewModel(
       comments: [],
+      unprocessedCommentsSet: {},
       rootNote: null,
     );
 
@@ -67,24 +41,32 @@ class EventFeedState
     // Clears the subscriptions.
     _rootNoteSub = null;
     _commentNotesSub = null;
-
-    // Disposes of the EventFeed instance.
-    await myEventFeed.dispose();
   }
 
   /// Called when the notifier is first built. Initializes the event feed and subscriptions.
   @override
   FeedEventViewModel build(String arg) {
-    // Ensures that resources are cleaned up when the notifier is disposed.
-    ref.onDispose(() {
-      resetStateDispose();
+    /// auto dispose delay
+    final link = ref.keepAlive();
+    Timer? timer;
+    //ref.onDispose(() => timer?.cancel());
+    ref.onCancel(() {
+      print("### EventFeedState canceled");
+      //todo: stop subscription
+      timer = Timer(Duration(minutes: 2), () => link.close());
+    });
+    ref.onResume(() {
+      // todo: resume subscription
+      print("### EventFeedState resumed");
+      timer?.cancel();
     });
 
-    // Sets up the EventFeed dependencies.
-    _setupEventFeed();
-
-    // Initializes subscriptions for the event feed.
-    _initSubscriptions();
+    // Ensures that resources are cleaned up when the notifier is disposed.
+    ref.onDispose(() {
+      print("### EventFeedState disposed");
+      resetStateDispose();
+      timer?.cancel();
+    });
 
     // Performs the initial fetch for the root note and its replies.
     _initialFetch(arg);
@@ -92,33 +74,43 @@ class EventFeedState
     // Returns the initial state of the event feed.
     return FeedEventViewModel(
       comments: [],
+      unprocessedCommentsSet: {},
       rootNote: null,
     );
   }
 
   /// Fetches the root note and its replies for the given rootNoteId.
   _initialFetch(String rootNoteId) {
+    final notesP = ref.watch(getNotesProvider);
     // Subscribes to updates for the root note.
-    myEventFeed.subscribeToRootNote(
-      noteId: rootNoteId,
-    );
 
-    // Subscribes to updates for the reply notes of the root note.
-    myEventFeed.subscribeToReplyNotes(
-      rootNoteId: rootNoteId,
-    );
-  }
-
-  /// Initializes subscriptions for the root note and comment notes streams.
-  void _initSubscriptions() async {
-    // Listens to updates from the root note stream and updates the state accordingly.
-    _rootNoteSub = myEventFeed.rootNoteStream.listen((event) {
-      state = state.copyWith(rootNote: event);
+    final sub = notesP.getNote(rootNoteId).listen((rootNote) {
+      // Updates the root note in the state.
+      state = state.copyWith(rootNote: rootNote);
     });
 
-    // Listens to updates from the replies tree stream and updates the state accordingly.
-    _commentNotesSub = myEventFeed.repliesTreeStream.listen((tree) {
-      state = state.copyWith(comments: tree);
+    // Subscribes to updates for the reply notes of the root note.
+    final repliesStream = notesP.genericNostrQuery(
+      requestId: "replies-${rootNoteId.substring(0, 12)}",
+      eTags: [rootNoteId],
+      kinds: [ndk_entities.Nip01Event.kTextNodeKind],
+      //limit: 10,
+    );
+
+    repliesStream
+        .bufferTime(const Duration(milliseconds: 700))
+        .where((events) => events.isNotEmpty)
+        .listen((replies) {
+      state = state.copyWith(
+        unprocessedCommentsSet: {...state.unprocessedCommentsSet, ...replies},
+        comments: EventFeed.buildRepliesTree(
+          rootNoteId: rootNoteId,
+          replies: [...state.unprocessedCommentsSet, ...replies],
+        ),
+      );
+    }).onDone(() {
+      /// setup subscription for new replies
+      print("### Replies stream done");
     });
   }
 }
