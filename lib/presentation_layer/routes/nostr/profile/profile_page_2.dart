c6e8817303195000739c617b5b444de1c46f7912
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-02-27 16:12:53 +0100

    fix: mobile padding

--- lib/presentation_layer/routes/nostr/profile/profile_page_2.dart
+++ lib/presentation_layer/routes/nostr/profile/profile_page_2.dart
@@ -45,7 +45,8 @@ class ProfilePage2 extends ConsumerWidget {
     return Scaffold(
       backgroundColor: Palette.background,
       body: GenericFeed(
-        feedPadding: EdgeInsets.only(top: 48),
+        feedPadding:
+            EdgeInsets.only(top: MediaQuery.of(context).padding.top + 48),
         additionalTabViews: [
           StarterPacksList(
             pubkey: pubkey,
