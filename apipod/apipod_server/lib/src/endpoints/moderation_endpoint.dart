e6ed0e019134c2c548b09de6a7d12ab82e70d2f0
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-03-15 10:52:52 +0100

    apipod setup

---
+++ apipod/apipod_server/lib/src/endpoints/moderation_endpoint.dart
@@ -0,0 +1,7 @@
+import 'package:serverpod/serverpod.dart';
+
+class ModerationEndpoint extends Endpoint {
+  Future<Map<String, dynamic>> getProfileBloomFilter(Session session) async {
+    return {};
+  }
+}
--- lib/data_layer/repositories/moderation_repository_impl.dart
+++ lib/data_layer/repositories/moderation_repository_impl.dart
@@ -1,10 +1,15 @@
 import '../../domain_layer/repositories/moderation_repository.dart';
+import '../data_sources/serverpod_data_source.dart';
 
 class ModerationRepositoryImpl implements ModerationRepository {
-  ModerationRepositoryImpl();
+  final ServerpodDataSource server;
+
+  ModerationRepositoryImpl({
+    required this.server,
+  });
 
   @override
   Future<Map<String, dynamic>> fetchBloomFilter() {
-    throw UnimplementedError();
+    return server.client.moderation.getProfileBloomFilter();
   }
 }
