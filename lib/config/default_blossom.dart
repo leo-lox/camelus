ef9ecd0065bdd2014c6dcf6675d7ed832008a056
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-03-18 10:37:27 +0100

    edit default blossom servers

--- lib/config/default_blossom.dart
+++ lib/config/default_blossom.dart
@@ -1,6 +1,6 @@
 const DEFAULT_BLOSSOM_SERVERS = [
+  'https://blossom.band',
   'https://nostr.download',
   'https://blossom.f7z.io',
   'https://blossom.primal.net',
-  'https://files.v0l.io',
 ];
