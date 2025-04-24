45215c9efe4fa178ec0a5e3f3ba55d86e55866bb
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-04-24 16:47:54 +0200

    add /user deep link

--- lib/lifecycle/deep_links.dart
+++ lib/lifecycle/deep_links.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:app_links/app_links.dart';
+import 'package:camelus/presentation_layer/providers/nip05_provider.dart';
 
 import 'package:riverpod/riverpod.dart';
 
@@ -21,10 +22,13 @@ Future<void> listenDeeplinks({
 // Subscribe to all events (initial link and further)
   final sub = appLinks.uriLinkStream.listen((uri) async {
     final uriString = uri.toString();
-    if (uriString.startsWith("nostr:")) {
-      _nostrLinks(
-        link: uriString,
-      );
+    if (uriString.startsWith(RegExp(r'nostr:(//)?'))) {
+      final nostrCode = uriString.replaceAll(RegExp(r'nostr:(//)?'), "");
+      _nostrDecode(nostrCode: nostrCode, providerContainer: providerContainer);
+    } else if (uriString.startsWith(RegExp(r'camelus:(//)?'))) {
+      final nostrCode = uriString.replaceAll(RegExp(r'camelus:(//)?'), "");
+
+      _nostrDecode(nostrCode: nostrCode, providerContainer: providerContainer);
     } else if (uriString.startsWith("https://camelus.app")) {
       await _camelusLinks(
         link: uriString,
@@ -71,15 +75,32 @@ Future<void> _camelusLinks({
     navigatorKey.currentState?.pushReplacementNamed(
       "/onboarding",
     );
+  } else if (myMatch.startsWith("/user/")) {
+    final List<String> myMatchSplit = myMatch.split("/");
+
+    final String username = myMatchSplit[2];
+
+    if (myMatchSplit.length > 3 && myMatchSplit[3] == "status") {
+      // This is a status URL
+      final String statusId = myMatchSplit[4];
+
+      _pushNote(
+        noteId: statusId,
+      );
+    } else {
+      // This is just a user profile URL
+      _nostrDecode(nostrCode: username, providerContainer: providerContainer);
+    }
   } else {
     print("camelus link not supported ${link}");
   }
 }
 
-_nostrLinks({
-  required String link,
-}) {
-  final myMatch = link.replaceAll("nostr:", "");
+_nostrDecode({
+  required String nostrCode,
+  required ProviderContainer providerContainer,
+}) async {
+  final myMatch = nostrCode;
   String myPubkeyHex = "";
 
   if (myMatch.contains("nprofile")) {
@@ -123,14 +144,21 @@ _nostrLinks({
     _pushNote(
       noteId: eventId,
     );
+  } else if (myMatch.contains("@")) {
+    final nip05P = providerContainer.read(nip05provider);
+    final nip05Data = await nip05P.get(myMatch);
+    if (nip05Data != null) {
+      _pushProfile(pubkey: nip05Data.pubkey);
+    }
   }
 }
 
 _pushProfile({
   required String pubkey,
 }) {
-  navigatorKey.currentState?.pushNamed(
+  navigatorKey.currentState?.pushNamedAndRemoveUntil(
     "/nostr/profile",
+    (route) => route.isFirst,
     arguments: pubkey,
   );
 }
@@ -138,8 +166,9 @@ _pushProfile({
 _pushNote({
   required String noteId,
 }) {
-  navigatorKey.currentState?.pushNamed(
+  navigatorKey.currentState?.pushNamedAndRemoveUntil(
     "/nostr/event",
+    (route) => route.isFirst,
     arguments: {
       "root": noteId,
       "scrollIntoView": null,
