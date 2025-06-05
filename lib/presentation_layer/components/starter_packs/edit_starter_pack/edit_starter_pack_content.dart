36097726bb3c2b38c2e79c9f80a47c889a6105f4
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-06-05 12:30:12 +0200

    person select card

--- lib/presentation_layer/components/starter_packs/edit_starter_pack/edit_starter_pack_content.dart
+++ lib/presentation_layer/components/starter_packs/edit_starter_pack/edit_starter_pack_content.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+import '../../../../config/palette.dart';
+import '../../../../domain_layer/entities/user_metadata.dart';
+import '../../../atoms/my_profile_picture.dart';
+import '../../../atoms/nip_05_text.dart';
+import '../../../providers/search_provider.dart';
+import '../../../routes/search_page.dart';
+import '../../person_card.dart';
+import '../../search_bar.dart';
 
 class EditStarterPackContent extends ConsumerStatefulWidget {
   const EditStarterPackContent({super.key});
@@ -12,8 +22,92 @@ class _EditStarterPackContentState
     extends ConsumerState<EditStarterPackContent> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("content"),
+    final searchService = ref.read(searchProvider);
+    final searchState = ref.watch(searchStateProvider);
+    return Scaffold(
+      backgroundColor: Palette.background,
+      body: Column(
+        children: [
+          SearchBarWidget(
+            onSearchChanged: (value) async {
+              ref.read(searchStateProvider.notifier).setSearchQuery(value);
+
+              if (value.isEmpty) {
+                ref
+                    .read(searchStateProvider.notifier)
+                    .clearSearch(stillSearching: false);
+                return;
+              }
+              final users = await searchService.searchMetadata(value);
+              ref
+                  .read(searchStateProvider.notifier)
+                  .setSearchResultsUsers(users);
+            },
+            onSubmit: (value) {},
+            helpSearch: (context) {},
+          ),
+          Expanded(
+              child: ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              ...searchState.searchResultsUsers
+                  .map((user) => PersonSelect(user: user)),
+            ],
+          ))
+        ],
+      ),
     );
   }
+}
+
+Widget PersonSelect({required UserMetadata user}) {
+  return ListTile(
+    onTap: () {
+      // Toggle selection
+    },
+    title: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        UserImage(
+          imageUrl: user?.picture,
+          pubkey: user.pubkey,
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                user?.name ?? "",
+                style: const TextStyle(
+                  color: Palette.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+              Nip05Text(
+                pubkey: user.pubkey,
+                nip05verified: user.nip05,
+              ),
+              const SizedBox(height: 4),
+              Text(
+                user?.about ?? "",
+                style: const TextStyle(
+                  color: Palette.gray,
+                  fontSize: 12,
+                ),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+    trailing: Icon(
+      false ? PhosphorIcons.checkCircle() : PhosphorIcons.circle(),
+      color: Palette.white,
+    ),
+  );
 }
