08808a540c6b4b6d544ecfb1f7c5a5018593b986
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-06-07 11:35:03 +0200

    starter pack card

---
+++ lib/presentation_layer/components/starter_packs/starter_pack_card.dart
@@ -0,0 +1,169 @@
+import 'package:camelus/presentation_layer/atoms/my_profile_picture.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../config/palette.dart';
+import '../../../domain_layer/entities/nostr_list.dart';
+import '../../../helpers/helpers.dart';
+import '../../../helpers/nprofile_helper.dart';
+import '../../atoms/overlapting_avatars.dart';
+import '../../providers/metadata_state_provider.dart';
+
+class StarterPackCard extends ConsumerStatefulWidget {
+  final NostrStarterPack pack;
+  const StarterPackCard({
+    super.key,
+    required this.pack,
+  });
+
+  @override
+  ConsumerState<StarterPackCard> createState() => _StarterPackCardState();
+}
+
+class _StarterPackCardState extends ConsumerState<StarterPackCard> {
+  @override
+  Widget build(BuildContext context) {
+    final creatorMetadata =
+        ref.watch(metadataStateProvider(widget.pack.pubKey)).userMetadata;
+    return Card(
+      margin: const EdgeInsets.all(16),
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // image
+              if (widget.pack.image != null)
+                Container(
+                  height: 120,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(widget.pack.image!),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              if (widget.pack.image == null)
+                Container(
+                  height: 120,
+                  color: Palette.extraDarkGray,
+                ),
+
+              // body - content
+              Padding(
+                padding: const EdgeInsets.fromLTRB(
+                    16, 32, 16, 16), // Increased top padding
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Title
+                    Text(
+                      widget.pack.title ?? "Starter Pack",
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    Text(
+                      widget.pack.description ?? "",
+                      maxLines: 3,
+                      textAlign: TextAlign.justify,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                        color: Palette.gray,
+                      ),
+                    ),
+
+                    const SizedBox(height: 8),
+
+                    // Creator info
+                    Row(
+                      children: [
+                        Text(
+                          "by",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Palette.gray,
+                          ),
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          creatorMetadata?.name ??
+                              _pubkeyToHrBech32Short(widget.pack.pubKey),
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Palette.gray,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        UserImage(
+                          size: 20,
+                          imageUrl: creatorMetadata?.picture,
+                          pubkey: widget.pack.pubKey,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          // avatars
+          Positioned(
+            top: 100,
+            left: 12,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 155,
+                  child: OverlappingAvatars(
+                    avatarSize: 40,
+                    avatars: widget.pack.pubKeys.take(5).map((e) {
+                      return UserImage(
+                        imageUrl: ref
+                            .watch(metadataStateProvider(e.value))
+                            .userMetadata
+                            ?.picture,
+                        pubkey: e.value,
+                      );
+                    }).toList(),
+                  ),
+                ),
+                // + counter
+                if ((widget.pack.pubKeys.length) > 5)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Text(
+                      "+${(widget.pack.pubKeys.length - 5)}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Palette.lightGray,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+_pubkeyToHrBech32Short(pubkey) {
+  final bech = Helpers().encodeBech32(pubkey, "npub");
+  final bechShort = NprofileHelper().bech32toHr(bech, cutLength: 11);
+
+  return bechShort;
+}
