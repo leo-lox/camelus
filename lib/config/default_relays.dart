8fb8158d42f0ace466dc6b88be94016219ac9a72
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-06-13 11:27:29 +0200

    edit default relays

--- lib/config/default_relays.dart
+++ lib/config/default_relays.dart
@@ -1,7 +1,7 @@
 import 'package:ndk/entities.dart' as ndk_entities;
 
 List<String> CAMELUS_BOOTSTRAP_RELAYS = [
-  "wss://strfry.iris.to",
+  "wss://nos.lol",
   "wss://relay.damus.io",
   "wss://relay.nostr.band",
   "wss://relay.snort.social",
@@ -14,12 +14,11 @@ final Map<String, ndk_entities.ReadWriteMarker>
     DEFAULT_ACCOUNT_CREATION_RELAYS = {
   // read
   "wss://relay.nostr.band": ndk_entities.ReadWriteMarker.readOnly,
-  "wss://nos.lol": ndk_entities.ReadWriteMarker.readOnly,
   "wss://nostr.wine": ndk_entities.ReadWriteMarker.readOnly,
   "wss://relay.mostr.pub": ndk_entities.ReadWriteMarker.readOnly,
-  "wss://countries.fiatjaf.com": ndk_entities.ReadWriteMarker.readOnly,
+  "wss://pyramid.fiatjaf.com": ndk_entities.ReadWriteMarker.readOnly,
   // read write
   "wss://relay.damus.io": ndk_entities.ReadWriteMarker.readWrite,
-  "wss://strfry.iris.to": ndk_entities.ReadWriteMarker.readWrite,
+  "wss://nos.lol": ndk_entities.ReadWriteMarker.readWrite,
   "wss://relay.snort.social": ndk_entities.ReadWriteMarker.readWrite,
 };
