58980bccd720ee2b9ee18676e0fa6b4fa7a90c67
1leo <58687994+1-leo@users.noreply.github.com>
2025-11-20 15:28:12 +0100

    chore: remove unused import

--- lib/presentation_layer/routes/search/search_page.dart
+++ lib/presentation_layer/routes/search/search_page.dart
@@ -9,7 +9,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../../../config/palette.dart';
 import '../../../domain_layer/entities/contact_list.dart';
 import '../../../domain_layer/entities/nostr_note.dart';
 import '../../../domain_layer/entities/user_metadata.dart';
