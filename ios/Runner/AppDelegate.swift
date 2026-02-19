80646342f6968782a79ca03169593b60a1d19e09
1leo <58687994+1-leo@users.noreply.github.com>
2026-02-19 17:51:03 +0100

    chore: init firebase native ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,9 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // if FirebaseApp.app() == nil {
-    //   FirebaseApp.configure()
-    // }
+    if FirebaseApp.app() == nil {
+      FirebaseApp.configure()
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
--- lib/lifecycle/notifications/background_notifications_thread.dart
+++ lib/lifecycle/notifications/background_notifications_thread.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:ndk/ndk.dart';
 import 'package:ndk_objectbox/ndk_objectbox.dart';
 import 'package:riverpod/riverpod.dart';
@@ -23,7 +24,7 @@ Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
   // check if already initialized (can happen if main thread is still alive in background)
   // doulbe init on iOS causes issues
-  if (Firebase.apps.isEmpty) {
+  if (Firebase.apps.isEmpty && defaultTargetPlatform != TargetPlatform.iOS) {
     try {
       await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
