51637d592de10cbd7b05c9723a1b9c54ea3b2fc9
1leo <58687994+1-leo@users.noreply.github.com>
2026-02-24 16:47:53 +0100

    remove: nostr band code

--- lib/domain_layer/repositories/nostr_band_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:camelus/domain_layer/entities/nostr_band_hashtags.dart';
-import 'package:camelus/domain_layer/entities/nostr_band_people.dart';
-
-abstract class NostrBandRepository {
-  Future<NostrBandPeople?> getTrendingProfiles();
-  Future<NostrBandHashtags?> getTrendingHashtags();
-}
